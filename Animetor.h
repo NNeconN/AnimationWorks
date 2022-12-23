100fad6c883f1423faffdefc0cd1e877f67f4096
DESKTOP-RMTGVGI\yuma1 <glpat-gKefB_XqYMf71BcGmyyS>
2022-12-24 08:15:02 +0900

    ssss

---
+++ Animetor.h
@@ -0,0 +1,28 @@
+#pragma once
+#pragma once
+#include "player.h"
+#include "CDirectInput.h"
+
+#include <vector>
+
+class Animetor
+{
+public:
+	Animetor(); //コンストラクタ
+
+	void Demo_DragPoints(); //線形保管されているグラフ
+
+	//線形補間
+	float lerp(float start, float end, float t) {
+		return start + t * (end - start);
+	}
+
+public:
+	std::vector<int> AnimetionKey{}; //アニメーションキー
+
+	Monster g_monster;
+private:
+	float sy; //leap()で計算後の数値を入れる変数 Y座標
+	float sx; //leap()で計算後の数値を入れる変数 X座標
+
+};
